0b514888de3edff401902ced32e35b926f7e0d3a
dam27 <dam2217@ic.ac.uk>
2019-11-14 12:03:20 +0000

    test

---
+++ src/test/java/TestServlet.java
@@ -0,0 +1,29 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class TestServlet {
+    @Mock
+    HttpServletRequest request;
+    @Mock
+    HttpServletResponse response;
+    @Before
+    public void setUp() throws Exception{
+        MockitoAnnotations.initMocks(this);
+    }
+    @Test
+    public void testDoGet() throws IOException, ServletException {
+        StringWriter stringWriter=new StringWriter();
+        PrintWriter printWriter=new PrintWriter(stringWriter);
+        when(response.getWriter()).thenReturn(printWriter);
+        when(request.getServletPath()).thenReturn("/patients");
+        MyServlet myServlet=new HospDB();
+        myServlet.doGet(request,response);
+        String output=stringWriter.getBuffer().toString();
+        Assert.assertThat(output,is(equalTo("Hello, World!")));
+    }
+}
