7b86bc557354174aa314c3cf243b2a4da2b36660
dam27 <dam2217@ic.ac.uk>
2019-11-14 11:47:21 +0000

    new

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class MyServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        resp.getWriter().write("Hello, world!");
     }
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
