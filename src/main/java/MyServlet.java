2e59ccb8898d0287c93cf5e55534588f55fc3331
dam2217 <dam2217@ic.ac.uk>
2020-01-02 13:56:41 +0000

    testing out the database connection

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -3,29 +3,52 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.sql.*;
 
-@WebServlet(urlPatterns={"/patients"}, loadOnStartup = 1)
+
+@WebServlet(urlPatterns={"/eczemadatabase"}, loadOnStartup = 1)
 
 public class MyServlet extends HttpServlet {
-    private static final Logger log= Logger.getLogger(MyServlet.class.getName());
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().write("Hello, world!");
+    public static void main(String[] args) throws Exception {
 
-        LogManager.getLogManager().readConfiguration(new FileInputStream("logging.properties"));
-
-        log.info("In constructor");
-        log.warning("Running low on memory");
-        log.severe("Can't allocate memory");
-
-
+        Connection connection = getConnection();
+        Statement stmt = connection.createStatement();
+        stmt.executeUpdate("DROP TABLE IF EXISTS ticks");
+        stmt.executeUpdate("CREATE TABLE ticks (tick timestamp)");
+        stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+        ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+        while (rs.next()) {
+            System.out.println("Read from DB: " + rs.getTimestamp("tick"));
+        }
     }
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
-            IOException {
+
+
+//    @Override
+//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+////        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+////        con.setRequestMethod("GET");
+//        resp.getWriter().write("Hello, world!");
+//    }
+//    @Override
+//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
+//            IOException {
+//        //database.write(location, req.body.frontBacksTATE)
+ //   }
+
+    //Ref 1: code from https://devcenter.heroku.com/articles/connecting-to-relational-databases-on-heroku-with-java
+    private static Connection getConnection() throws URISyntaxException, SQLException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+        return DriverManager.getConnection(dbUrl, username, password);
     }
+    //end of reference 1
 }
