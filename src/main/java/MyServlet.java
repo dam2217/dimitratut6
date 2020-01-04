7f53a47d35a79a41e394b166355ff9ea4d1ca73f
dam2217 <dam2217@ic.ac.uk>
2020-01-04 23:19:53 +0000

    Trying to see if Hello world prints

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -78,12 +78,10 @@ public class MyServlet extends HttpServlet {
 
 
 
-//    @Override
-//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-////        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-////        con.setRequestMethod("GET");
-//        resp.getWriter().write("Hello, world!");
-//    }
+   @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("Hello, world!");
+    }
 //    @Override
 //    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
 //            IOException {
