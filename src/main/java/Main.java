73588fdd2f55ac9626fbd7c84227d47990147448
dam2217 <dam2217@ic.ac.uk>
2020-01-05 20:22:23 +0000

    Trying to see if i can get info from existing table in database

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,6 @@
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Main {
 //    try {     // Registers the driver
@@ -17,18 +14,25 @@ public class Main {
         Statement stmt = connection.createStatement();
 
         String sqlStr;
+        sqlStr = "SELECT * FROM logdata WHERE date=1";
 //        try {
-            sqlStr = "CREATE TABLE appdata (id SERIAL PRIMARY  KEY, " +
-                    "hf varchar(16), hb varchar(16), tf varchar(16), tb varchar(16)," +
-                    "raf varchar(16), rab varchar(16), laf varchar(16), lab varchar(16)," +
-                    "rlf varchar(16), rlb varchar(16), llf varchar(16), llb varchar(16)," +
-                    "treatmentYorN varchar(6), treatmentUsed varchar(128), temperature varchar(6)," +
-                    "pollutionLevel varchar(20), pollenLevel varchar(20), hfTreated varchar(20)," +
-                    "hbTreated varchar(20), tfTreated varchar(20), tbTreated varchar(20)," +
-                    "rafTreated varchar(20), rabTreated varchar(20), lafTreated varchar(20)," +
-                    "labTreated varchar(20), rlfTreated varchar(20), rlbTreated varchar(20)," +
-                    "llfTreated varchar(20), llbTreated varchar(20), notes varchar(200))";
-            stmt.execute(sqlStr);
+//            sqlStr = "CREATE TABLE appdata (id SERIAL PRIMARY  KEY, " +
+//                    "hf varchar(16), hb varchar(16), tf varchar(16), tb varchar(16)," +
+//                    "raf varchar(16), rab varchar(16), laf varchar(16), lab varchar(16)," +
+//                    "rlf varchar(16), rlb varchar(16), llf varchar(16), llb varchar(16)," +
+//                    "treatmentYorN varchar(6), treatmentUsed varchar(128), temperature varchar(6)," +
+//                    "pollutionLevel varchar(20), pollenLevel varchar(20), hfTreated varchar(20)," +
+//                    "hbTreated varchar(20), tfTreated varchar(20), tbTreated varchar(20)," +
+//                    "rafTreated varchar(20), rabTreated varchar(20), lafTreated varchar(20)," +
+//                    "labTreated varchar(20), rlfTreated varchar(20), rlbTreated varchar(20)," +
+//                    "llfTreated varchar(20), llbTreated varchar(20), notes varchar(200))";
+           // stmt.execute(sqlStr);
+
+        ResultSet rset=stmt.executeQuery(sqlStr);
+        while(rset.next()){
+            System.out.println(rset.getInt("date")+" "+ rset.getString("headfront"));
+        }
+        rset.close();
 //        } catch (Exception e) {
 //        }
     }
