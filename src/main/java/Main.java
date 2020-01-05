cfe00d944f9882c050f67c40de7054749f40010c
dam2217 <dam2217@ic.ac.uk>
2020-01-05 11:20:43 +0000

    Made database table creation in different class to see if it works

---
+++ src/main/java/Main.java
@@ -0,0 +1,47 @@
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Main {
+//    try {     // Registers the driver
+//            Class.forName("org.postgresql.Driver");
+//        }
+//    catch{ Exception e}
+
+
+    public static void main(String[] args) throws Exception {
+        Connection connection = getConnection();
+        Statement stmt = connection.createStatement();
+
+        String sqlStr;
+        try {
+            sqlStr = "CREATE TABLE appdata (id SERIAL PRIMARY  KEY, " +
+                    "hf varchar(16), hb varchar(16), tf varchar(16), tb varchar(16)," +
+                    "raf varchar(16), rab varchar(16), laf varchar(16), lab varchar(16)," +
+                    "rlf varchar(16), rlb varchar(16), llf varchar(16), llb varchar(16)," +
+                    "treatmentYorN varchar(6), treatmentUsed varchar(128), temperature varchar(6)," +
+                    "pollutionLevel varchar(20), pollenLevel varchar(20), hfTreated varchar(20)," +
+                    "hbTreated varchar(20), tfTreated varchar(20), tbTreated varchar(20)," +
+                    "rafTreated varchar(20), rabTreated varchar(20), lafTreated varchar(20)," +
+                    "labTreated varchar(20), rlfTreated varchar(20), rlbTreated varchar(20)," +
+                    "llfTreated varchar(20), llbTreated varchar(20), notes varchar(200))";
+            stmt.execute(sqlStr);
+        } catch (Exception e) {
+        }
+    }
+    //Ref 1: code from https://devcenter.heroku.com/articles/connecting-to-relational-databases-on-heroku-with-java
+    private static Connection getConnection() throws URISyntaxException, SQLException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+        //  String dbUrl = System.getenv("JDBC_DATABASE_URL");
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+        return DriverManager.getConnection(dbUrl);
+    }
+    //end of reference 1
+}
+
+
--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -15,89 +15,89 @@ import java.sql.*;
 
 public class MyServlet extends HttpServlet {
 
-   // public static void main(String[] args) throws Exception {
-   {
-       try {     // Registers the driver
-           Class.forName("org.postgresql.Driver");
-       } catch (Exception e) {
-       }
-   }
-        Connection connection;
-    {
-        try {
-            connection = getConnection();
-        } catch (URISyntaxException ex) {
-            ex.printStackTrace();
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
-    }
-        Statement stmt;
-    {
-        try {
-            stmt = connection.createStatement();
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
-    }
-
-    public String sqlStr;
-
-    {
-        try {
-            sqlStr = "CREATE TABLE appdata (id SERIAL PRIMARY  KEY, " +
-                    "hf varchar(16), hb varchar(16), tf varchar(16), tb varchar(16)," +
-                    "raf varchar(16), rab varchar(16), laf varchar(16), lab varchar(16)," +
-                    "rlf varchar(16), rlb varchar(16), llf varchar(16), llb varchar(16)," +
-                    "treatmentYorN varchar(6), treatmentUsed varchar(128), temperature varchar(6)," +
-                    "pollutionLevel varchar(20), pollenLevel varchar(20), hfTreated varchar(20)," +
-                    "hbTreated varchar(20), tfTreated varchar(20), tbTreated varchar(20)," +
-                    "rafTreated varchar(20), rabTreated varchar(20), lafTreated varchar(20)," +
-                    "labTreated varchar(20), rlfTreated varchar(20), rlbTreated varchar(20)," +
-                    "llfTreated varchar(20), llbTreated varchar(20), notes varchar(200))";
-            stmt.execute(sqlStr);
-        } catch (Exception e) {
-        }
-    }
-//        try{
-//            sqlStr = "INSERT INTO appdata (hf,llf,raf) values('Mild', 'Severe', 'Moderate') ";
+//   // public static void main(String[] args) throws Exception {
+//   {
+//       try {     // Registers the driver
+//           Class.forName("org.postgresql.Driver");
+//       } catch (Exception e) {
+//       }
+//   }
+//        Connection connection;
+//    {
+//        try {
+//            connection = getConnection();
+//        } catch (URISyntaxException ex) {
+//            ex.printStackTrace();
+//        } catch (SQLException ex) {
+//            ex.printStackTrace();
+//        }
+//    }
+//        Statement stmt;
+//    {
+//        try {
+//            stmt = connection.createStatement();
+//        } catch (SQLException ex) {
+//            ex.printStackTrace();
+//        }
+//    }
+//
+//    public String sqlStr;
+//
+//    {
+//        try {
+//            sqlStr = "CREATE TABLE appdata (id SERIAL PRIMARY  KEY, " +
+//                    "hf varchar(16), hb varchar(16), tf varchar(16), tb varchar(16)," +
+//                    "raf varchar(16), rab varchar(16), laf varchar(16), lab varchar(16)," +
+//                    "rlf varchar(16), rlb varchar(16), llf varchar(16), llb varchar(16)," +
+//                    "treatmentYorN varchar(6), treatmentUsed varchar(128), temperature varchar(6)," +
+//                    "pollutionLevel varchar(20), pollenLevel varchar(20), hfTreated varchar(20)," +
+//                    "hbTreated varchar(20), tfTreated varchar(20), tbTreated varchar(20)," +
+//                    "rafTreated varchar(20), rabTreated varchar(20), lafTreated varchar(20)," +
+//                    "labTreated varchar(20), rlfTreated varchar(20), rlbTreated varchar(20)," +
+//                    "llfTreated varchar(20), llbTreated varchar(20), notes varchar(200))";
 //            stmt.execute(sqlStr);
-//        }catch (Exception e){
+//        } catch (Exception e) {
 //        }
-
-//        stmt.executeUpdate("DROP TABLE IF EXISTS ticks");
-//        stmt.executeUpdate("CREATE TABLE ticks (tick timestamp)");
-//        stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-//        ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-//        while (rs.next()) {
-//            System.out.println("Read from DB: " + rs.getTimestamp("tick"));
-//        }
-    //}
-
-
-
-
-
-   @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().write("Hello, world!");
-    }
-//    @Override
-//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
-//            IOException {
-//        //database.write(location, req.body.frontBacksTATE)
- //   }
-
-    //Ref 1: code from https://devcenter.heroku.com/articles/connecting-to-relational-databases-on-heroku-with-java
-    private static Connection getConnection() throws URISyntaxException, SQLException {
-        URI dbUri = new URI(System.getenv("DATABASE_URL"));
-
-      //  String dbUrl = System.getenv("JDBC_DATABASE_URL");
-        String username = dbUri.getUserInfo().split(":")[0];
-       String password = dbUri.getUserInfo().split(":")[1];
-       String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
-
-        return DriverManager.getConnection(dbUrl);
-    }
-    //end of reference 1
+//    }
+////        try{
+////            sqlStr = "INSERT INTO appdata (hf,llf,raf) values('Mild', 'Severe', 'Moderate') ";
+////            stmt.execute(sqlStr);
+////        }catch (Exception e){
+////        }
+//
+////        stmt.executeUpdate("DROP TABLE IF EXISTS ticks");
+////        stmt.executeUpdate("CREATE TABLE ticks (tick timestamp)");
+////        stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+////        ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+////        while (rs.next()) {
+////            System.out.println("Read from DB: " + rs.getTimestamp("tick"));
+////        }
+//    //}
+//
+//
+//
+//
+//
+//   @Override
+//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        resp.getWriter().write("Hello, world!");
+//    }
+////    @Override
+////    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
+////            IOException {
+////        //database.write(location, req.body.frontBacksTATE)
+// //   }
+//
+//    //Ref 1: code from https://devcenter.heroku.com/articles/connecting-to-relational-databases-on-heroku-with-java
+//    private static Connection getConnection() throws URISyntaxException, SQLException {
+//        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+//
+//      //  String dbUrl = System.getenv("JDBC_DATABASE_URL");
+//        String username = dbUri.getUserInfo().split(":")[0];
+//       String password = dbUri.getUserInfo().split(":")[1];
+//       String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+//
+//        return DriverManager.getConnection(dbUrl);
+//    }
+//    //end of reference 1
 }
