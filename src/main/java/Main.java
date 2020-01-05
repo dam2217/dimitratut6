4f969d9ad5b7b1a70a9986e12981dfa1f65440f7
dam2217 <dam2217@ic.ac.uk>
2020-01-05 11:24:33 +0000

    Made database table creation in different class to see if it works

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -39,7 +39,7 @@ public class Main {
         String username = dbUri.getUserInfo().split(":")[0];
         String password = dbUri.getUserInfo().split(":")[1];
         String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
-        return DriverManager.getConnection(dbUrl);
+        return DriverManager.getConnection(dbUrl, username,password);
     }
     //end of reference 1
 }
