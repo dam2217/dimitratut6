a16ddc0b7abb4da4f582ba8d0963a437f2db990a
dam2217 <dam2217@ic.ac.uk>
2020-01-10 16:52:24 +0000

    working!! Testing JSON

---
+++ src/main/java/LogEntrySerial.java
@@ -0,0 +1,79 @@
+import java.io.Serializable;
+
+public class LogEntrySerial implements Serializable {
+    public CharSequence hf;
+    public CharSequence hb;
+    public CharSequence tf;
+    public CharSequence tb;
+    public CharSequence raf;
+    public CharSequence rab;
+    public CharSequence laf;
+    public CharSequence lab;
+    public CharSequence rlf;
+    public CharSequence rlb;
+    public CharSequence llf;
+    public CharSequence llb;
+    public String treatmentYorN;
+    public String treatmentUsed;
+    public String temperature;
+    public String humidity;
+    public String pollutionLevel;
+    public String pollenLevel;
+    public String location;
+    public String hfTreated;
+    public String hbTreated;
+    public String tfTreated;
+    public String tbTreated;
+    public String rafTreated;
+    public String rabTreated;
+    public String lafTreated;
+    public String labTreated;
+    public String rlfTreated;
+    public String rlbTreated;
+    public String llfTreated;
+    public String llbTreated;
+    public String notes;
+
+    public LogEntrySerial(CharSequence headf, CharSequence headb, CharSequence torsof,
+                          CharSequence torsob, CharSequence rarmf,
+                          CharSequence rarmb, CharSequence larmf, CharSequence larmb,
+                          CharSequence rlegf, CharSequence rlegb,
+                          CharSequence llegf, CharSequence llegb, String treatmentyesorno,
+                          String treatmentu, String temp, String hum, String pollution,
+                          String pollen, String loc, String hft, String hbt, String tft, String tbt,
+                          String raft, String rabt, String laft, String labt, String rlft,
+                          String rlbt, String llft, String llbt, String n){
+        this.hf = headf;
+        this.hb = headb;
+        this.tf = torsof;
+        this.tb = torsob;
+        this.raf = rarmf;
+        this.rab = rarmb;
+        this.laf = larmf;
+        this.lab = larmb;
+        this.rlf = rlegf;
+        this.rlb = rlegb;
+        this.llf = llegf;
+        this.llb = llegb;
+        this.treatmentYorN = treatmentyesorno;
+        this.treatmentUsed = treatmentu;
+        this.temperature = temp;
+        this.humidity = hum;
+        this.pollutionLevel = pollution;
+        this.pollenLevel = pollen;
+        this.location = loc;
+        this.hfTreated = hft;
+        this.hbTreated = hbt;
+        this.tfTreated = tft;
+        this.tbTreated = tbt;
+        this.rafTreated = raft;
+        this.rabTreated = rabt;
+        this.lafTreated = laft;
+        this.labTreated = labt;
+        this.rlfTreated = rlft;
+        this.rlbTreated = rlbt;
+        this.llfTreated = llft;
+        this.llbTreated = llbt;
+        this.notes = n;
+    }
+}
--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -1,12 +1,16 @@
+import com.google.gson.Gson;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.sql.*;
+import java.util.logging.LogManager;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
@@ -19,7 +23,7 @@ public class MyServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//
+
         resp.getWriter().write("Hello, world!");
 
 
@@ -28,9 +32,14 @@ public class MyServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
             IOException {
         String reqBody=req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+        Gson gson = new Gson();
+        LogEntrySerial test = gson.fromJson(reqBody, LogEntrySerial.class);
+        LogManager.getLogManager().readConfiguration(new FileInputStream("logging.properties"));
         resp.setContentType("text/html");
         resp.getWriter().write("Thank you client!");
+        req.getServletPath();
         log.info(reqBody);
+        System.out.println(test.location);
 //        String reqBody=req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
 //        resp.setContentType("text/html");
 //        log.info(reqBody);
