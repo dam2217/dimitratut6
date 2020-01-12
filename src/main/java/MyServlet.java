6b23e1c03dd8d5f98bd5dab4866df9cc0471194f
dam2217 <dam2217@ic.ac.uk>
2020-01-12 19:07:23 +0000

    testing sending 2 full strings across

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -23,7 +23,7 @@ public class MyServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String datasend = null;
+        String datasend = "";
         try{ Connection conn =  getConnection();
             Statement s=conn.createStatement();
             String sqlStr = "SELECT * FROM userdata WHERE id<3";
