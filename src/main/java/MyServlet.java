7c39e054df74d8282199df0d40b12586202c1c28
dam2217 <dam2217@ic.ac.uk>
2020-01-12 19:26:13 +0000

    testing string

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -23,7 +23,7 @@ public class MyServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String datasend = "";
+        String datasend = "dick";
         try{ Connection conn =  getConnection();
             Statement s=conn.createStatement();
             String sqlStr = "SELECT * FROM userdata WHERE id<3";
